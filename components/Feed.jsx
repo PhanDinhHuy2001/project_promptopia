7309602be0c041262342130ed02037bde4e50852
Phan Dinh Huy <huydinhphan2001@gmail.com>
2023-07-15 13:38:10 +0700

    fix feed page #2

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -24,14 +24,15 @@ const Feed = () => {
 
   const handleSearchChange = (e) => {};
 
-  const fetchPosts = async () => {
-    const response = await fetch("/api/prompt");
-    const data = await response.json();
-
-    setPosts(data);
-  };
-
   useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+
+      setPosts(data);
+    };
+    console.log(posts);
+
     fetchPosts();
   }, []);
 
